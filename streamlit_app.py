7dcd9152f9bef0641524c217e433e8e99a1132fe
ypei11 <ypei11@syr.edu>
2025-08-29 00:32:27 +0000

    Formatting changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,9 @@ st.write(
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
-# Ask user for their OpenAI API key via st.text_input. 
+# Ask user for their OpenAI API key via 'st.text_input'. 
 # Alternatively, you can store the API key in ./.streamlit/secrets.toml and access it 
-# via st.secrets, see https://docs.streamlit.io/develop/concepts/connections/secrets-management 
+# via 'st.secrets', see https://docs.streamlit.io/develop/concepts/connections/secrets-management 
 
 openai_api_key = st.text_input("OpenAI API Key", type="password")
 if not openai_api_key:
@@ -31,12 +31,12 @@ if not openai_api_key:
 else:
     client = OpenAI(api_key=openai_api_key)
 
-# Let the user upload a file via st.file_uploader.
+    # Let the user upload a file via st.file_uploader.
     uploaded_file = st.file_uploader(
         "Upload a document (.txt or .md or .pdf)", type=("txt", "md", "pdf")
     )
 
-# Ask the user for a question via st.text_area.
+    # Ask the user for a question via st.text_area.
     question = st.text_area(
         "Now ask a question about the document!",
         placeholder="Can you give me a short summary?",
@@ -61,11 +61,11 @@ else:
             {"role": "user", 
              "content": f"Here's a document:\n\n{document}\n\n---\n\n{question}"}
         ]
-# Generate an answer using the OpenAI API.
+        # Generate an answer using the OpenAI API.
         stream = client.chat.completions.create(
             model="gpt-5-nano",
             messages=messages,
             stream=True,
         )
-# Stream the response to the app using st.write_stream.
+        # Stream the response to the app using st.write_stream.
         st.write_stream(stream)
